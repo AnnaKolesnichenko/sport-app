cfd10ecc10fcba9744d6c94878dd8cf408b44373
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2024-03-18 12:39:59 +0300

    added

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import SingleReview from "./SingleReview";
-import { TfiPlus } from "react-icons/tfi";
-import arrowR from "../../images/Arrow_1.svg";
-import arrowL from "../../images/Arrow_2.svg";
-import "./Reviews.scss";
-import "./SingleReview.scss";
+import React from 'react';
+import SingleReview from './SingleReview';
+import { TfiPlus } from 'react-icons/tfi';
+import arrowR from '../../images/Arrow_1.svg';
+import arrowL from '../../images/Arrow_2.svg';
+import './Reviews.scss';
+import './SingleReview.scss';
 
 const Reviews = () => {
   return (
     <div className="container">
       <div className="review-about">
         <p className="reviews-title">
-          Відгуки про товар <span>(11)</span>{" "}
+          Відгуки про товар <span>(11)</span>{' '}
         </p>
 
         <div className="pagination">
-          {" "}
+          {' '}
           <span>01 / 03</span>
           <div>
             <img src={arrowL} alt="left" />
