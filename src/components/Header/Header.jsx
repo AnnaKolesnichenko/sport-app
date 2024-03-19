a894eaf169ab2453585653525aecdf0cc6e40bd4
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2024-03-19 16:32:08 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,62 +6,64 @@ import search from '../../images/search.svg';
 const Header = () => {
   return (
     <header>
-      <div className="authentication">
-        <div className="help-section">
-          <ul>
-            <li>
-              <span>098 900 09 09</span>
-            </li>
-            <li>
-              <a href="/">Допомога</a>
-            </li>
-          </ul>
-        </div>
-        <div className="auth">
-          <ul>
-            <li>
-              <a href="/">Увійти</a>
-            </li>
-            <li>
-              <span>/</span>
-            </li>
-            <li>
-              <a href="/">Зареєструватися</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="navigation">
-        <div className="logo">
-          <a href="/">IGNAT</a>
-        </div>{' '}
-        <div>
-          <nav>
-            <ul className="menu-items">
+      <div style={{ width: '1366px' }}>
+        <div className="authentication">
+          <div className="help-section">
+            <ul>
               <li>
-                <a href="/">новинки</a>
+                <span>098 900 09 09</span>
               </li>
               <li>
-                <a href="/">чоловіки</a>
-              </li>
-              <li>
-                <a href="/">жінки</a>
-              </li>
-              <li>
-                <a href="/">аксесуари</a>
-              </li>
-              <li>
-                <a href="/">акції</a>
+                <a href="/">Допомога</a>
               </li>
             </ul>
-          </nav>
+          </div>
+          <div className="auth">
+            <ul>
+              <li>
+                <a href="/">Увійти</a>
+              </li>
+              <li>
+                <span>/</span>
+              </li>
+              <li>
+                <a href="/">Зареєструватися</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="extra-nav">
-          <input type="text" />
-          <img src={search} alt="search" className="search-icon" />
-          {/* <RiSearch2Line className="search-icon" /> */}
-          <Icons />
+
+        <div className="navigation">
+          <div className="logo">
+            <a href="/">IGNAT</a>
+          </div>{' '}
+          <div>
+            <nav>
+              <ul className="menu-items">
+                <li>
+                  <a href="/">новинки</a>
+                </li>
+                <li>
+                  <a href="/">чоловіки</a>
+                </li>
+                <li>
+                  <a href="/">жінки</a>
+                </li>
+                <li>
+                  <a href="/">аксесуари</a>
+                </li>
+                <li>
+                  <a href="/">акції</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="extra-nav">
+            <input type="text" />
+            <img src={search} alt="search" className="search-icon" />
+            {/* <RiSearch2Line className="search-icon" /> */}
+            <Icons />
+          </div>
         </div>
       </div>
     </header>
